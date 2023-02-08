f598181c299f34b57a35da5c4bfc54abf7d60574
Debnarayan <debnarayanduttaz663@gmail.com>
2023-02-09 02:49:30 +0530

    solution

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,16 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+  
+  const [user,setUser] = useState()
+
+  function clickHandler(){
+    setUser("Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy")
+  }
   return (
     <div id="main">
-      // Do not alter the main div
+      <p id="para">{user}</p>
+      <button id="click" onClick={clickHandler}>Click</button>
     </div>
   );
 }
